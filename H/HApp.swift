59a7ac5d7090432cbf59a071d1219be70554ea2a
weodsa <we.od.sa@gmail.com>
2024-10-21 09:25:40 +0300

    Initial Commit

---
+++ H/HApp.swift
@@ -0,0 +1,17 @@
+//
+//  HApp.swift
+//  H
+//
+//  Created by Ohoud Aldossari on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HTests/HTests.swift
@@ -0,0 +1,17 @@
+//
+//  HTests.swift
+//  HTests
+//
+//  Created by Ohoud Aldossari on 18/04/1446 AH.
+//
+
+import Testing
+@testable import H
+
+struct HTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
